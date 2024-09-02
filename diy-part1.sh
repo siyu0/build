3799afd538b8103fc5a116a41d054a6693ef7978
siyu0 <xuqiuxisb@163.com>
2024-09-03 07:41:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,7 @@
 # Add a feed source 
 # sed -i "/helloworld/d" "feeds.conf.default"
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # switch kernal version
